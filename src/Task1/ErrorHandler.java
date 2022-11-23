89c5efcae30839cdcc204333e75afc7191c8181c
Alex <Chugun777akk@yandex.ru>
2022-11-23 21:29:30 +0700

    add class ErrorsHandler

--- src/Task1/ErrorHandler.java
+++ src/Task1/ErrorHandler.java
@@ -1,4 +1,13 @@
 package Task1;
 
 public class ErrorHandler {
+    public void handler(Number num)
+    {
+        try{
+            num.inputData();
+        }catch (NumberFormatException e){
+            System.out.println("Ввод символов недопустим, введите число ");
+        }
+
+    }
 }
