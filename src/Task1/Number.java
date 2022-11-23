26343d4301e0b69c501a16ea481745dcaea98ef8
Alex <Chugun777akk@yandex.ru>
2022-11-23 19:57:25 +0700

    add method input and get value

--- src/Task1/Number.java
+++ src/Task1/Number.java
@@ -1,4 +1,24 @@
 package Task1;
 
+import java.util.Scanner;
+
 public class Number {
+    private float value;
+
+    public float getValue() {
+        return value;
+    }
+    public float inputData()
+    {
+        System.out.print("Введите значение: ");
+        Scanner sc = new Scanner(System.in);
+        value  = sc.nextFloat();
+        return value;
+    }
+
+    public static void main(String[] args) {
+        Number num1  = new Number();
+        num1.inputData();
+        System.out.println(num1.getValue());
+    }
 }
