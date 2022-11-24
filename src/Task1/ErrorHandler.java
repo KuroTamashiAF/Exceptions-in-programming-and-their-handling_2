a0b990c4bba72d02bf952d8adc7dfd878c423e3e
Alex <Chugun777akk@yandex.ru>
2022-11-24 19:17:18 +0700

    Create class Errorhandler

--- src/Task1/ErrorHandler.java
+++ src/Task1/ErrorHandler.java
@@ -1,14 +1,18 @@
 package Task1;
 
+import java.util.InputMismatchException;
+
 public class ErrorHandler {
-    public void handler(Number num)
+    public void handler()
     {
+        Number num = new Number();
         try{
             num.inputData();
-        }catch (NumberFormatException e){
+        }catch (InputMismatchException e){
             System.out.println("Ввод символов недопустим, введите число ");
             num.inputData();
         }
+        System.out.printf("Вы ввели %f",num.getValue());
 
     }
 }
--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,4 +1,12 @@
 package Task1;
 
 public class Main {
+    public static void appRun()
+    {
+        ErrorHandler run  = new ErrorHandler();
+        run.handler();
+    }
+    public static void main(String[] args) {
+        appRun();
+    }
 }
