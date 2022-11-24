47f5efa6094f60802fdb83e53a1dd7a0195eec82
Alex <Chugun777akk@yandex.ru>
2022-11-24 20:37:43 +0700

    Task is done

--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -1,4 +1,17 @@
 package Task2;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        int [] intArray = {65,23,36,478,500,6589,745,87896,9105,14560};
+        try {
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+
+    }
 }
