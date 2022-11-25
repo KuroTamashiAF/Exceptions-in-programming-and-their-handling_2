2e564527d9bbc79de926be9e6afad2c827377d0b
Alex <Chugun777akk@yandex.ru>
2022-11-25 15:13:53 +0700

    create Noname class

--- src/Task4/Noname.java
+++ src/Task4/Noname.java
@@ -1,4 +1,22 @@
 package Task4;
 
+import java.util.Scanner;
+
 public class Noname {
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+    public void inputData()
+    {
+        Scanner sc  = new Scanner(System.in);
+        this.data = sc.next();
+    }
+
+    public static void main(String[] args) {
+        Noname num = new Noname();
+        num.inputData();
+        System.out.println(num.getData());
+    }
 }
