bd87bda260025f4b2eb8eafcb5eb1efbdc814f85
Alex <Chugun777akk@yandex.ru>
2022-11-26 11:53:09 +0700

    add method checked

--- src/Task4/ErrorsHandler.java
+++ src/Task4/ErrorsHandler.java
@@ -1,4 +1,22 @@
 package Task4;
 
 public class ErrorsHandler {
+
+    public void checked()
+    {
+        Noname obj1 = new Noname();
+        while(true){
+            try{
+                obj1.inputData();
+
+                if (obj1.getData().length() == 0 )
+                {
+                    throw new RuntimeException();
+                }
+                break;
+            }catch (RuntimeException e) {
+                System.out.println("Нельзя вводить пустые строки, повторите ввод: ");
+            }
+        }
+    }
 }
